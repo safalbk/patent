019cd90d78d6c552cc2334030cb1950afe367f4d
Safal-BK <safal.bk@bolgatty.nl>
2023-02-21 15:10:32 +0530

    added cv new database

--- analysis/models.py
+++ analysis/models.py
@@ -8,3 +8,10 @@ class Profile(models.Model):
     age = models.PositiveBigIntegerField()
     def __str__(self):
         return str(self.name)
+
+class CompanyDataTable(models.Model):
+    company_name = models.CharField(max_length=1000)
+    city = models.CharField(max_length=500)
+    country = models.CharField(max_length=50)
+    patent_no = models.CharField(max_length=20)
+    kind = models.CharField(max_length=10)
--- int.sh
+++ int.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-source ../env2/bin/activate ; export DATABASE_URL=postgres://patentdb_user:mPdLVphnZJhYNAUBNdQNMo4wy44dHiSd@dpg-cfo8vairrk0fd9vn2ne0-a.oregon-postgres.render.com/patentdb;
+export DATABASE_URL=postgres://patentdb_user:mPdLVphnZJhYNAUBNdQNMo4wy44dHiSd@dpg-cfo8vairrk0fd9vn2ne0-a.oregon-postgres.render.com/patentdb;
